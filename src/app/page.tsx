ea48cb4b0d1fb7c8f3b13739c327f7951b666b23
Sthefane <sthefanedias04@gmail.com>
2025-04-16 17:44:33 -0300

    ajustes no card charmander

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,9 @@ export default function Home() {
     <h1 className="text-4xl font-bold text-amber-50 ">POKEDEX</h1>
     <p className="text-2xl text-gray-300">A simple Pokedex app</p>
 
-    <div className="cards-pokedex border-1 rounded-lg p-30 bg-white">
-      <div className="cards-pokedex border-1 rounded-lg p-30 bg-gray-300">
+    <div className="cards-pokedex w-  border-1 rounded-lg p-10 bg-white flex flex-row">
+      <div className="cards-pokemons border-1 rounded-lg border-none p-18 bg-gray-300">
+
       <Image
         src="/charmander.png"
         alt="charmander"
@@ -17,10 +18,11 @@ export default function Home() {
         height={100}
         className="rounded-lg"
       />
+      <h3 className='font-medium text-gray-500 '>N° 004</h3>
+      <h2 className="text-2xl font-bold text-gray-700 ">Charmander</h2>
+      <button className='border-1 border-none rounded-sm w-full bg-[#fd7d24] text-white text-sm'>Fogo</button>
       </div>
       
-      
-
     </div>
   </div>
   
